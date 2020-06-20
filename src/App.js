5c74a9ed687efd56158baafb6384a279a52b5ef6
salmanqazi <salmanqazi@users.noreply.github.com>
2020-06-20 23:05:11 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> Hello World -- React App -- </h1>
+      <h1> Hello World -- React App ---- </h1>
      </div>
   );
 }
